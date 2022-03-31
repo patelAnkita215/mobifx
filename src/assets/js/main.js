1d9b8da30692e72be165a331939870ea82171a49
patelAnkita215 <patelankita960@gmail.com>
2022-03-31 13:50:19 +0530

    change on sidemenu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -41,50 +41,7 @@
         }
     });
 
-    // =========== OWL Product Category
-    // $(document).ready(function() {
-    //     // $(".owl-carousel").owlCarousel();
-        
-    //     //Load global header & footer
-    //     $(function () {
-    //         fetch("./header.html")
-    //             .then(response => {
-    //                 return response.text()
-    //             })
-    //             .then(data => {
-    //                 $("#header").html(data);
-    //                 manageMobileNavMenu();
-    //             });
-
-    //         fetch("./footer.html")
-    //             .then(response => {
-    //                 return response.text()
-    //             })
-    //             .then(data => {
-    //                 document.querySelector("footer").innerHTML = data;
-    //             });
-    //     }); 
-    // });
-    
-    /* - Magnific Popup ===== */
-    if ($(".vidya-image").length) {
-        var url;
-        $(".vidya-image").magnificPopup({
-            delegate: "a.zoom",
-            type: "image",
-            tLoading: "Loading image #%curr%...",
-            mainClass: "mfp-img-mobile",
-            gallery: {
-                enabled: true,
-                navigateByImgClick: false,
-                preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
-            },
-            image: {
-                tError: ("<a href=" % url) % ">The image #%curr%</a> could not be loaded.",
-            },
-        });
-    }
-
+   
     
     // Toggle .header-scrolled class to #header when page is scrolled
     $(window).scroll(function () {
@@ -102,72 +59,11 @@
         $("#topbar").addClass("topbar-scrolled");
     }
 
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $(".back-to-top").fadeIn("slow");
-        } else {
-            $(".back-to-top").fadeOut("slow");
-        }
-    });
-
-    $(".back-to-top").click(function () {
-        $("html, body").animate({
-                scrollTop: 0,
-            },
-            500, "easeInOutExpo"
-        );
-        return false;
-    });
+    
 
       
 
-    // Init AOS
-    function aos_init() {
-        AOS.init({
-            duration: 1000,
-            once: true,
-        });
-    }
-    $(window).on("load", function () {
-        aos_init();
-    });
-    
-    // ========= right-side fixed form
-    $("#inquiry_form").click(function () {
-        $(".b-notification-bar").toggleClass("active");
-        if ($(".b-notification-bar .inq").hasClass('info-box1')) {
-            $(".b-notification-bar .inq").removeClass("info-box1");
-            $("#inquiry_form").removeClass("contact")
-        } else {
-            $(".b-notification-bar .inq").addClass("info-box1");
-            $("#inquiry_form").addClass("contact")
-        }
-        $("#rfp_form").removeClass("submitrfp");
-        $(".b-notification-bar .rfp").removeClass("info-box2")
-    });
-    $(".closeInquiry").click(function () {
-        $(".b-notification-bar").removeClass("active");
-        $(".b-notification-bar .inq").removeClass("info-box1");
-        $("#inquiry_form").removeClass("contact")
-    });
-    $(".closeRfp").click(function () {
-        $(".b-notification-bar").removeClass("active");
-        $(".b-notification-bar .rfp").removeClass("info-box2");
-        $("#rfp_form").removeClass("submitrfp")
-    });
-    $("#rfp_form").click(function () {
-        $(".b-notification-bar").toggleClass("active");
-        $(".b-notification-bar .inq").removeClass("info-box1");
-        $("#inquiry_form").removeClass("contact");
-        if ($(".b-notification-bar .rfp").hasClass("info-box2")) {
-            $(".b-notification-bar .rfp").removeClass("info-box2");
-            $("#rfp_form").removeClass("submitrfp")
-        } else {
-            $(".b-notification-bar .rfp").addClass("info-box2");
-            $("#rfp_form").addClass("submitrfp")
-        }
-    });
+     
 
     function manageMobileNavMenu() {
         // Mobile Navigation
@@ -206,13 +102,6 @@
         }
     }
 
-    /*---- image hover 3D effect ------*/
-    var tilt = $('.js-tilt');
-        if(tilt.length) {
-        const tilt = $('.js-tilt').tilt();
-    }
-    /* Fully year */
-    // var date = new Date().getFullYear();document.getElementById("year").innerHTML = date;  
-
+    
 })(jQuery);   
     
