5c7776ecbaec11bbf3654fdf6a07cd1f19e0aa0d
pintumaurya <pintumaurya0245@gmail.com>
2022-04-02 15:22:46 +0530

    email verification

--- src/app/components/email-verify/email-verify.component.ts
+++ src/app/components/email-verify/email-verify.component.ts
@@ -11,11 +11,14 @@ export class EmailVerifyComponent implements OnInit {
 
   token: any = "a66cecfcf84e7b010ec58b0f4be3427ad9ad6747535c75c4446d7bdd0324ac8e";
   // urlParams: any = {};
+  email: any;
 
   constructor(
     public _authService: ApiService,
     public route: ActivatedRoute,
-  ) { }
+  ) {
+    this.email = localStorage.getItem('email');
+  }
 
   ngOnInit(): void {
     // this.urlParams.token = this.route.snapshot.queryParamMap.get('token');
